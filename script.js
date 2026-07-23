6b26184ea92cebfdd6a9acd83242b0d56bb7f967
ARMANPK1 <pakistan07026@gmail.com>
2026-07-23 03:30:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,8 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";https://hpmabasscvxobqjiaxya.supabase.co
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";sb_publishable_Q6fekn1-CYNPC7kbjdX8zg_8-XUkcNB
+const SUPABASE_URL = "https://hpmabasscvxobqjiaxya.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_Q6fekn1-CYNPC7kbjdX8-XUkcNB";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
-
-async function uploadFile() {
-  const fileInput = document.getElementById("fileInput");
-
-  if (!fileInput.files.length) {
-    alert("একটি ফাইল নির্বাচন করুন");
-    return;
-  }
-
-  const file = fileInput.files[0];
-
-  const { error } = await supabase.storage
-    .from("documents")
-    .upload(file.name, file, {
-      upsert: true
-    });
-
-  if (error) {
-    alert("Upload Failed: " + error.message);
-  } else {
-    alert("✅ Upload সফল হয়েছে!");
-    loadFiles();
-  }
-}
-
-async function loadFiles() {
-  const fileList = document.getElementById("fileList");
-  fileList.innerHTML = "";
-
-  const { data, error } = await supabase.storage
-    .from("documents")
-    .list("", {
-      limit: 100
-    });
-
-  if (error) return;
-
-  data.forEach(file => {
-    const li = document.createElement("li");
-    li.textContent = file.name;
-    fileList.appendChild(li);
-  });
-}
-
-loadFiles();
