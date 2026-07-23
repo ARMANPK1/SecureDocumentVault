acf11103e7c5fa627d3f1916d8cba8418afbbf86
ARMANPK1 <pakistan07026@gmail.com>
2026-07-23 05:02:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert("Script loaded");
+alert("JS OK");
 const SUPABASE_URL = "https://hpmabasscvxobqjiaxya.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_Q6fekn1-CYNPC7kbjdX8zg_8-XUkcNB";
