7a2035d3aa94a0beebd593e1181fcb403845488a
ARMANPK1 <pakistan07026@gmail.com>
2026-07-26 02:45:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert("script.js শুরু হয়েছে");
+alert("Script File is Working!");
 
 const SUPABASE_URL = "https://hpmabasscvxobqjiaxya.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_Q6fekn1-CYNPC7kbjdX8zg_8-XUkcNB";
