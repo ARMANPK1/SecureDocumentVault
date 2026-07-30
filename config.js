9f5b38bc7acff61eb4a528c42ea3412d4b219778
ARMANPK1 <pakistan07026@gmail.com>
2026-07-30 04:13:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 // Supabase Credentials
 const SUPABASE_URL ='https://hpmabasscvxobqjiaxya.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_Q6fekn1-CYNPC7kbjdX8zg_8-XUkcNB';
-
+// Supabase Client জেনারেট করা
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 // Initialize Supabase Client
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
