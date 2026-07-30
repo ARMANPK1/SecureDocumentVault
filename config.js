8ad2d64cbaad4f9d0a56779bc853ed8d81ad3086
ARMANPK1 <pakistan07026@gmail.com>
2026-07-31 01:25:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://your-project.supabase.co';
-const SUPABASE_KEY = 'your-anon-key';
+const SUPABASE_URL = https://hpmabasscvxobqjiaxya.supabase.co;
+const SUPABASE_KEY = 'sb_publishable_Q6fekn1-CYNPC7kbjdX8zg_8-XUkcNB';
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
