bc245b39f2e79fad1b4f2c44f8056dbce888f320
ARMANPK1 <pakistan07026@gmail.com>
2026-08-01 02:07:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const SUPABASE_URL = "https://hpmabasscvxobqjiaxya.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_Q6fekn1-CYNPC7kbjdX8zg_8-XUkcNB";
+const SUPABASE_ANON_KEY = 'sb_publishable_Q6fekn1-CYNPC7kbjdX8zg_8-XUkcNB';
