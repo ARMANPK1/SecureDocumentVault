2da937151d92a20794dcb8277a461b7addac1ee5
ARMANPK1 <pakistan07026@gmail.com>
2026-08-06 10:01:26 +0200

    Fix formatting of SUPABASE_ANON_KEY assignment

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 const SUPABASE_URL = "https://hpmabasscvxobqjiaxya.supabase.co";
 
 // আপনার Supabase Dashboard > Settings > API থেকে কপি করা আসল `anon` `public` key (eyJ... দিয়ে শুরু)
-const SUPABASE_ANON_KEY = "'sb_publishable_Q6fekn1-CYNPC7kbjdX8zg_8-XUkcNB';
+const SUPABASE_ANON_KEY ='sb_publishable_Q6fekn1-CYNPC7kbjdX8zg_8-XUkcNB';
 // Supabase Initializer (সব পেজের জন্য স্ট্যান্ডার্ড নাম)
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 const _supabase = supabaseClient; // ব্যাকওয়ার্ড সাপোর্ট এর জন্য
